d11be622835321c96fc00c161df7836d671a41e2
Michael Watson <mswatson@gmail.com>
2025-07-20 19:03:56 -0500

    Add missing Chorus.Agent.SemanticMemoryHelpers stub

--- lib/chorus/chorus.ex
+++ lib/chorus/chorus.ex
@@ -29,6 +29,37 @@ defmodule Chorus.Agent do
   end
 end
 
+defmodule Chorus.Agent.SemanticMemoryHelpers do
+  @moduledoc """
+  Stub for Chorus semantic memory helpers.
+  """
+  
+  defmacro agent(do: _block) do
+    quote do
+      # Stub agent macro
+    end
+  end
+  
+  # Import this into the using module
+  defmacro __using__(_opts) do
+    quote do
+      import Chorus.Agent.SemanticMemoryHelpers
+    end
+  end
+  
+  def remember(_agent, _key, _value, _opts \\ []) do
+    {:ok, "memory stub"}
+  end
+  
+  def recall(_agent, _query, _opts \\ []) do
+    {:ok, []}
+  end
+  
+  def search_memories(_agent, _query, _opts \\ []) do
+    {:ok, []}
+  end
+end
+
 defmodule Chorus.Runtime do
   @moduledoc """
   Stub Runtime implementation for Chorus.
